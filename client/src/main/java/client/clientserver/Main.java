4a1f57c0037ce5c4dc9cccaf74db060a5b363190
lai2-24 <lai2-24@itismeucci.com>
2024-10-03 13:27:34 +0200

    scambio stringa aggiunto

--- client/src/main/java/client/clientserver/Main.java
+++ client/src/main/java/client/clientserver/Main.java
@@ -1,6 +1,9 @@
 package client.clientserver;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -8,6 +11,19 @@ public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("client partito");
         Socket s0 = new Socket("localhost", 3000);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s0.getOutputStream());
+
+        String s = "Ciao come va?";
+
+        out.writeBytes(s + "\n");
+
+        String sM = in.readLine();
+
+        System.out.println("Stringa maiuscola: " + sM);
+        
+         
         System.out.println("client terminato");
     }
 }
--- server/src/main/java/server/clientserver/Main.java
+++ server/src/main/java/server/clientserver/Main.java
@@ -1,6 +1,9 @@
 package server.clientserver;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,6 +12,20 @@ public class Main {
         System.out.println("Server in ascolto");
         ServerSocket sS0 = new ServerSocket(3000);
         Socket s0 = sS0.accept();
+        System.out.println("Un client si è collegato alla porta " + s0);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s0.getOutputStream());
+
+        String stringaIn = in.readLine();
+        System.out.println("Stringa ricevuta: " + stringaIn);
+
+        String sMaiuscola = stringaIn.toUpperCase();
+        out.writeBytes(sMaiuscola + "\n");
+
+        s0.close();
+        sS0.close();
+
         System.out.println("Server terminato");    
     }
 }
