f1e0c5903971447c9d7bdb94284fc0611b6c6cfc
lai2-24 <lai2-24@itismeucci.com>
2024-10-10 13:26:11 +0200

    aggiunta feature multi-client

--- server/src/main/java/server/clientserver/Main.java
+++ server/src/main/java/server/clientserver/Main.java
@@ -11,24 +11,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Server in ascolto");
         ServerSocket sS0 = new ServerSocket(3000); //Porta dove il server aspetta richiesta
-        Socket s0 = sS0.accept(); //quando arriva una connessione, viene accettata e rende la nuova porta su cui avverra il vero passaggio di dati
-        System.out.println("Un client si è collegato alla porta " + s0.getPort());
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream())); //stream dati in entrata
-        DataOutputStream out = new DataOutputStream(s0.getOutputStream()); //stream dati inn uscita
-        String stringaIn;
-        
         do {
-            stringaIn = in.readLine(); //attendo che arrivi una stringa sullo stream
-            System.out.println("Stringa ricevuta: " + stringaIn);
-
-            String sMaiuscola = stringaIn.toUpperCase(); //trasformo la stringa in maiuscola
-            out.writeBytes(sMaiuscola + "\n"); //rispondo al client inviando la stringa
-        } while (!stringaIn.equals("!")); //se il client manda "!", il server si chiude
-
-        s0.close();
-        sS0.close();
-
-        System.out.println("Server terminato");    
+            Socket s0 = sS0.accept(); //quando arriva una connessione, viene accettata e rende la nuova porta su cui avverra il vero passaggio di dati   
+            gestioneServizio gS = new gestioneServizio(s0);
+            gS.start();
+        } while(true);
     }
 }
