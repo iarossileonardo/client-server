c1342cb9cec4061ca854054591d639655db6088c
lai2-24 <lai2-24@itismeucci.com>
2024-10-10 12:41:15 +0200

    aggiunti commenti

--- client/src/main/java/client/clientserver/Main.java
+++ client/src/main/java/client/clientserver/Main.java
@@ -11,25 +11,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("client partito");
-        Socket s0 = new Socket("localhost", 3000);
+        Socket s0 = new Socket("localhost", 3000); //socker che dice indirizzo e porta del server a cui connetersi
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream()));
-        DataOutputStream out = new DataOutputStream(s0.getOutputStream());
-        Scanner input = new Scanner(System.in);
+        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream())); //stream dati in
+        DataOutputStream out = new DataOutputStream(s0.getOutputStream()); //stream dati out
+
+        Scanner input = new Scanner(System.in); //scanner input da tastiera
         String s;
 
         do {
             System.out.println("Inserire la stringa da inviare (! per chiudere la connessione)\n");
-            s = input.nextLine();
+            s = input.nextLine(); //prendo la linea in input con lo scanner
             
-            out.writeBytes(s + "\n");
+            out.writeBytes(s + "\n"); //invio i dati al server
             
-            String sM = in.readLine();
+            String sM = in.readLine(); //leggo i dati che arrivano dal server
             
             System.out.println("Invio dati a server");
             
             System.out.println("Stringa maiuscola: " + sM);
-        } while (!s.equals("!"));
+        } while (!s.equals("!")); //se scrivo "!" esco dal ciclo e si chiude il client
         
         input.close();
         s0.close();
--- server/src/main/java/server/clientserver/Main.java
+++ server/src/main/java/server/clientserver/Main.java
@@ -10,21 +10,21 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Server in ascolto");
-        ServerSocket sS0 = new ServerSocket(3000);
-        Socket s0 = sS0.accept();
+        ServerSocket sS0 = new ServerSocket(3000); //Porta dove il server aspetta richiesta
+        Socket s0 = sS0.accept(); //quando arriva una connessione, viene accettata e rende la nuova porta su cui avverra il vero passaggio di dati
         System.out.println("Un client si è collegato alla porta " + s0.getPort());
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream()));
-        DataOutputStream out = new DataOutputStream(s0.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(s0.getInputStream())); //stream dati in entrata
+        DataOutputStream out = new DataOutputStream(s0.getOutputStream()); //stream dati inn uscita
         String stringaIn;
         
         do {
-            stringaIn = in.readLine();
+            stringaIn = in.readLine(); //attendo che arrivi una stringa sullo stream
             System.out.println("Stringa ricevuta: " + stringaIn);
 
-            String sMaiuscola = stringaIn.toUpperCase();
-            out.writeBytes(sMaiuscola + "\n");
-        } while (!stringaIn.equals("!"));
+            String sMaiuscola = stringaIn.toUpperCase(); //trasformo la stringa in maiuscola
+            out.writeBytes(sMaiuscola + "\n"); //rispondo al client inviando la stringa
+        } while (!stringaIn.equals("!")); //se il client manda "!", il server si chiude
 
         s0.close();
         sS0.close();
